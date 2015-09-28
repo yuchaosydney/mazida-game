6e7753b06c732dcb85fa1fb44f454a9fb6da1357
YUCHAO WU <yuchao.sydneyuni@gmail.com>
2015-09-28 11:10:02 +1000

    picking rgb color works

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var game = new Phaser.Game(1000, 700, Phaser.CANVAS, 'game',{ preload: preload, create: create, update: update });
 
+var bg, bitBg;
 var TO_RADIANS = Math.PI/180;
 var speed = 1;
 var maxSpeed = 4;
@@ -15,8 +16,11 @@ function preload() {
 }
 
 function create() {
-  game.add.image(0, 0, 'track-hit');
-  car = game.add.sprite(500,500, 'car');
+  bg = game.add.image(0, 0, 'track-hit');
+  bitBg = game.make.bitmapData();
+  bitBg.load(bg);
+  getRgbByXY(500,600);
+  car = game.add.sprite(500,650, 'car');
   com_car = game.add.sprite(600,500, 'car');
   
   game.physics.enable(car, Phaser.Physics.ARCADE);
@@ -25,14 +29,14 @@ function create() {
   car.anchor.setTo(0.5,0.5);
   car.rotationStep = 4;
   car.angle = 0;
-  car.top_left = new Array(car.x,car.y,0);
-  car.top_right = new Array(car.x + car_width, car.y,0);
-  car.center_point = new Array(car.top_left[0] + car_width/2,car.top_left[1] + car_height/2,0);//(center_x,centery_y, angle) 
-  car.top_left[2] = Math.atan2(car.top_left[0] - car.center_point[0],car.center_point[1] - car.top_left[1]); 
-  car.top_right[2] = Math.atan2(car.top_right[0] - car.center_point[0],car.center_point[1] - car.top_right[1]); 
+  car.top_left = {x:car.x,y:car.y,angle:0};
+  car.top_right = {x:car.x + car_width,y:car.y,angle:0};
+
+  car.center_point = {x:car.x + car_width/2,y:car.y + car_height/2};
+
+  car.top_left.angle = Math.atan2(car.top_left.x - car.center_point.x,car.center_point.y - car.top_left.y); 
+  car.top_right.angle = Math.atan2(car.top_right.x - car.center_point.x,car.center_point.y - car.top_right.y); 
  
-  console.log((car.center_point[0] + car_radius*Math.sin(car.top_right[2] + Math.PI/2)));
-  console.log((car.center_point[1] + car_radius*Math.cos(car.top_right[2] + Math.PI/2)*-1));
   
   game.physics.enable(com_car, Phaser.Physics.ARCADE);
   com_car.body.collideWorldBounds = true;
@@ -46,8 +50,6 @@ function create() {
 
 function update() {
 
-  game.physics.arcade.collide(car, com_car);
-  
   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
   {
     steerLeft(car);
@@ -56,7 +58,8 @@ function update() {
   {
     steerRight(car);
   }
-
+  car_top_left_point_update(car);
+  car_top_right_point_update(car);
   //moveCar(car);
   //moveCar(com_car);
 }
@@ -89,13 +92,25 @@ function speedXY (rotation, speed) {
 }
 
 function steerLeft(car){
-  console.log(car.rotation); 
   car.angle -= car.rotationStep * (speed/maxSpeed);
 }
 
 function steerRight(car){
-  console.log(car.rotation); 
   car.angle += car.rotationStep * (speed/maxSpeed);
 }
 
+function car_top_left_point_update(car) {
+  car.top_left.x = car.center_point.x + car_radius*Math.sin(car.top_left.x + car.angle); 
+  car.top_left.y = car.center_point.y + car_radius*Math.cos(car.top_left.y + car.angle)*-1; 
+}
 
+function car_top_right_point_update(car) {
+  car.top_right.x = car.center_point.x + car_radius*Math.sin(car.top_right.x + car.angle); 
+  car.top_right.y = car.center_point.y + car_radius*Math.cos(car.top_right.y + car.angle)*-1;
+}
+
+//return hex color format
+function getRgbByXY(x,y) {
+  var rgbObj = bitBg.getPixelRGB(x, y);
+  return Phaser.Color.RGBtoString(rgbObj.r,rgbObj.g,rgbObj.b);
+}
